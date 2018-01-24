f18d3e11b12f903ed3d02607878e7c5b73011996
MaitreMenard <maxime.menard.1@ulaval.ca>
2018-01-23 23:28:44 -0500

    Abstract class Stat

---
+++ Assets/Scripts/Entities/Stats/Stat.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public abstract class Stat : MonoBehaviour
+{
+    [SerializeField]
+    protected float baseValue;
+    [SerializeField]
+    protected float currentValue;
+    [SerializeField]
+    protected float flatBonus;
+    [SerializeField]
+    protected float percentBonus;
+    [SerializeField]
+    protected float flatMalus;
+    [SerializeField]
+    protected float percentMalus;
+	
+    public float GetBaseValue()
+    {
+        return baseValue;
+    }
+
+    public void SetBaseValue(float baseValue)
+    {
+        this.baseValue = baseValue;
+        UpdateCurrentValue();
+    }
+
+    public float GetCurrentValue()
+    {
+        return currentValue;
+    }
+
+    public float GetFlatBonus()
+    {
+        return flatBonus;
+    }
+
+    public void SetFlatBonus(float flatBonus)
+    {
+        this.flatBonus = flatBonus;
+        UpdateCurrentValue();
+    }
+
+    public float GetPercentBonus()
+    {
+        return percentBonus;
+    }
+
+    public void SetPercentBonus(float percentBonus)
+    {
+        this.percentBonus = percentBonus;
+        UpdateCurrentValue();
+    }
+
+    public virtual void UpdateCurrentValue()
+    {
+        currentValue = (baseValue + flatBonus) * (1 + percentBonus) * (1 - percentMalus) - flatMalus;
+    }
+}
