8354f387dea7310984bee920b5392ecae1fea8b1
MaitreMenard <maxime.menard.1@ulaval.ca>
2018-02-06 11:15:11 -0500

    Created a Buff class

---
+++ Assets/Scripts/Entities/Buff.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Buff
+{
+    private float durationInSeconds;
+    private float stopTime;
+
+    public delegate void OnApplyEffect(Entity entity);
+    public OnApplyEffect OnApply;
+
+    public delegate void OnUpdateEffect(Entity entity);
+    public OnUpdateEffect OnUpdate;
+
+    public delegate void OnRemoveEffect(Entity entity);
+    public OnRemoveEffect OnRemove;
+
+
+    public Buff(float durationInSeconds)
+    {
+        this.durationInSeconds = durationInSeconds;
+        stopTime = 0;
+    }
+
+    public virtual void ApplyEffectTo(Entity entity)
+    {
+        OnApply(entity);
+        stopTime = Time.time + durationInSeconds;
+    }
+
+    public virtual void Update(Entity entity)
+    {
+        OnUpdate(entity);
+    }
+
+    public virtual void RemoveEffectFrom(Entity entity)
+    {
+        OnRemove(entity);
+    }
+
+    public virtual bool HasExpired()
+    {
+        return Time.time > stopTime;
+    }
+}
