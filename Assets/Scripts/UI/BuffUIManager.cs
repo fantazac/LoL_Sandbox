34f09608fda1d0ed4a39ad38eeb440b3fca48123
TOUR-ALEX\Alexandre <fantazacmec1@hotmail.com>
2018-02-13 13:08:52 -0500

    Update to buffs and debuffs for clarity

--- Assets/Scripts/UI/BuffUIManager.cs
+++ Assets/Scripts/UI/BuffUIManager.cs
@@ -9,17 +9,13 @@ public class BuffUIManager : MonoBehaviour
     private GameObject buffOutlinePrefab;
 
     private Dictionary<Buff, GameObject> buffOutlines;
-    private Dictionary<Buff, Image> antiClockwiseImages;
-    private Dictionary<Buff, Image> clockwiseImages;
+    private Dictionary<Buff, Image> darkImages;
     private Dictionary<Buff, Text> buffStacksTexts;
-
-    private const float IMAGE_FILL_RATIO = 0.98f;
 
     private BuffUIManager()
     {
         buffOutlines = new Dictionary<Buff, GameObject>();
-        antiClockwiseImages = new Dictionary<Buff, Image>();
-        clockwiseImages = new Dictionary<Buff, Image>();
+        darkImages = new Dictionary<Buff, Image>();
         buffStacksTexts = new Dictionary<Buff, Text>();
     }
 
@@ -29,16 +25,14 @@ public class BuffUIManager : MonoBehaviour
         buffOutline.transform.SetParent(transform, false);
         buffOutlines.Add(buff, buffOutline);
 
-        Image[] buffImages = buffOutline.transform.GetChild(0).GetComponentsInChildren<Image>();
-        buffImages[1].sprite = buffSprite;
-        buffImages[2].sprite = buffSprite;
-        antiClockwiseImages.Add(buff, buffImages[1]);
-        clockwiseImages.Add(buff, buffImages[2]);
+        buffOutline.transform.GetChild(0).GetComponent<Image>().sprite = buffSprite;
+        Image darkImage = buffOutline.transform.GetChild(0).GetComponent<Image>();
+        darkImages.Add(buff, buffOutline.transform.GetChild(0).GetChild(0).GetComponent<Image>());
         buffStacksTexts.Add(buff, buffOutline.transform.GetComponentInChildren<Text>());
         
         if (!buff.HasDuration)
         {
-            buffImages[0].fillAmount = 1;
+            darkImages[buff].fillAmount = 0;
         }
     }
 
@@ -49,8 +43,7 @@ public class BuffUIManager : MonoBehaviour
 
     public void UpdateBuffDuration(Buff buff, float duration, float durationRemaining, int buffStacks)
     {
-        antiClockwiseImages[buff].fillAmount = (durationRemaining / duration) * IMAGE_FILL_RATIO;
-        clockwiseImages[buff].fillAmount = (1 - (durationRemaining / duration)) * IMAGE_FILL_RATIO;
+        darkImages[buff].fillAmount = durationRemaining / duration;
         if(buffStacks > 0)
         {
             buffStacksTexts[buff].text = "" + buffStacks;
@@ -59,8 +52,7 @@ public class BuffUIManager : MonoBehaviour
 
     public void RemoveExpiredBuff(Buff buff)
     {
-        antiClockwiseImages.Remove(buff);
-        clockwiseImages.Remove(buff);
+        darkImages.Remove(buff);
         buffStacksTexts.Remove(buff);
 
         GameObject expiredBuffOutline = buffOutlines[buff];
