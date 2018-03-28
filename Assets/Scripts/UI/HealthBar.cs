2d8e33c47addfb0f7586d01a6c30ce6c1f6026ce
TOUR-ALEX\Alexandre <fantazacmec1@hotmail.com>
2018-03-28 18:08:26 -0400

    HEALTH BARS (start)

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    private Transform characterToFollow;
+
+    private Vector2 healthBarOffset;
+    private Vector2 characterXOffset;
+    private Vector2 characterYOffset;
+    private Vector2 characterYOffset2;
+
+    private Camera characterCamera;
+    private Rect canvas;
+
+    private void Start()
+    {
+        characterCamera = StaticObjects.CharacterCamera;
+        canvas = GetComponentInParent<Canvas>().GetComponent<RectTransform>().rect;
+        healthBarOffset = Vector2.right * canvas.width * -0.5f + Vector2.up * canvas.height * -0.5f;
+        characterXOffset = Vector2.right * GetComponent<RectTransform>().rect.width * -0.25f;
+        characterYOffset = Vector2.up * 120;
+        characterYOffset2 = Vector2.up * -40;
+    }
+
+    public void SetupHealthBar(Character character)
+    {
+        characterToFollow = character.transform;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 position = characterCamera.WorldToScreenPoint(characterToFollow.position);
+        GetComponent<RectTransform>().anchoredPosition =
+            Vector2.right * (position.x * 2 + healthBarOffset.x) +
+            Vector2.up * (position.y * 2 + healthBarOffset.y) +
+            characterXOffset +
+            characterYOffset +
+            characterYOffset2 * ((position.y * 2) / canvas.height);
+    }
+}
---
+++ Assets/Scripts/UI/HealthBarManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject healthBarPrefab;
+
+    private List<Character> characters;
+    private List<GameObject> healthBars;
+
+    private HealthBarManager()
+    {
+        characters = new List<Character>();
+        healthBars = new List<GameObject>();
+    }
+
+    private void Start()
+    {
+        foreach (Character character in FindObjectsOfType<Character>())
+        {
+            characters.Add(character);
+
+            GameObject healthBar = Instantiate(healthBarPrefab);
+            healthBar.GetComponent<HealthBar>().SetupHealthBar(character);
+            healthBar.transform.SetParent(transform, false);
+            healthBars.Add(healthBar);
+        }
+    }
+}
