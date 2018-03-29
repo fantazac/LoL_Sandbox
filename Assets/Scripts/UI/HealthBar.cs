4ffaed94942d8d3277b0e1b3a50716e549513f9b
TOUR-ALEX\Alexandre <fantazacmec1@hotmail.com>
2018-03-29 14:36:21 -0400

    Health bar small update

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    [SerializeField]
+    private Image healthImage;
+    [SerializeField]
+    private Image resourceImage;
+    [SerializeField]
+    private Text levelText;
+
     private Transform characterToFollow;
 
     private Vector2 healthBarOffset;
-    private Vector2 characterXOffset;
     private Vector2 characterYOffset;
-    private Vector2 characterYOffset2;
 
     private Camera characterCamera;
     private Rect canvas;
@@ -17,11 +23,8 @@ public class HealthBar : MonoBehaviour
     private void Start()
     {
         characterCamera = StaticObjects.CharacterCamera;
-        //canvas = GetComponentInParent<Canvas>().GetComponent<RectTransform>().rect;
         healthBarOffset = Vector2.right * Screen.width * -0.5f + Vector2.up * Screen.height * -0.5f;
-        characterXOffset = Vector2.right * GetComponent<RectTransform>().rect.width * -0.5f;
-        characterYOffset = Vector2.up * 120;//((120 * Screen.height) / 1080);
-        characterYOffset2 = Vector2.up * -40;
+        characterYOffset = Vector2.up * 120;
     }
 
     public void SetupHealthBar(Character character)
@@ -35,8 +38,6 @@ public class HealthBar : MonoBehaviour
         GetComponent<RectTransform>().anchoredPosition =
             Vector2.right * (position.x + healthBarOffset.x) +
             Vector2.up * (position.y + healthBarOffset.y) +
-            //characterXOffset +
-            //characterYOffset2 * ((position.y) / canvas.height) +
             characterYOffset;
     }
 }
