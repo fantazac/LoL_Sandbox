65f253ecd263e1c02ce16e28901e53f8caad50e7
TOUR-ALEX\Alexandre <fantazacmec1@hotmail.com>
2018-04-09 13:14:35 -0400

    Beginning of crowd control, putting it on pause to start making MissFortune

---
+++ Assets/Scripts/Entities/Character/CharacterStatusManager.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStatusManager : MonoBehaviour
+{
+    private Character character;
+
+    private List<CrowdControlEffects> crowdControlEffectsOnCharacter;
+
+    public bool CanUseBasicAttacks { get; private set; }
+    public bool CanUseCharacterAbilities { get; private set; }
+    public bool CanUseMovement { get; private set; }
+    public bool CanUseSummonerAbilities { get; private set; }
+    public bool IsBlinded { get; private set; }
+
+    private CharacterStatusManager()
+    {
+        crowdControlEffectsOnCharacter = new List<CrowdControlEffects>();
+    }
+
+    private void Start()
+    {
+        character = GetComponent<Character>();
+    }
+
+    public void AddCrowdControlEffectOnCharacter(CrowdControlEffects crowdControlEffect)
+    {
+        crowdControlEffectsOnCharacter.Add(crowdControlEffect);
+    }
+
+    public void RemoveCrowdControlEffectFromCharacter(CrowdControlEffects crowdControlEffect)
+    {
+        crowdControlEffectsOnCharacter.Remove(crowdControlEffect);
+    }
+
+    /* !(crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.BLIND) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.CHARM) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.CRIPPLE) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.DISARM) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.DISRUPT) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.DROWSY) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.ENTANGLE) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.FEAR) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.GROUND) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.KNOCKASIDE) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.KNOCKBACK) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.KNOCKDOWN) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.KNOCKUP) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.NEARSIGHT) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.PACIFY) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.POLYMORPH) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.PULL) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.ROOT) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.SILENCE) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.SLEEP) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.SLOW) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.STASIS) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.STUN) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.SUPPRESION) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.SUSPENSION) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.TAUNT));
+                */
+
+    private void SetCanUseBasicAttacks()
+    {
+        CanUseBasicAttacks = !(crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.CHARM) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.DISARM) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.ENTANGLE) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.FEAR) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.KNOCKASIDE) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.KNOCKBACK) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.KNOCKUP) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.PACIFY) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.POLYMORPH) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.PULL) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.SLEEP) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.STASIS) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.STUN) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.SUPPRESION) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.SUSPENSION) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.TAUNT));
+    }
+
+    private void SetCanUseCharacterAbilities()
+    {
+        CanUseCharacterAbilities = !(crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.CHARM) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.FEAR) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.KNOCKASIDE) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.KNOCKBACK) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.KNOCKUP) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.PACIFY) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.POLYMORPH) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.PULL) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.SILENCE) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.SLEEP) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.STASIS) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.STUN) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.SUPPRESION) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.SUSPENSION) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.TAUNT));
+    }
+
+    private void SetCanUseMovement()
+    {
+        CanUseMovement = !(crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.CHARM) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.ENTANGLE) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.FEAR) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.KNOCKASIDE) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.KNOCKBACK) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.KNOCKUP) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.PULL) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.ROOT) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.SLEEP) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.STASIS) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.STUN) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.SUPPRESION) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.SUSPENSION) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.TAUNT));
+    }
+
+    private void SetCanUseSummonerAbilities()
+    {
+        CanUseSummonerAbilities = !(crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.PACIFY) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.POLYMORPH) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.SILENCE) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.STASIS) ||
+                crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.SUPPRESION));
+    }
+
+    private void SetIsBlinded()
+    {
+        IsBlinded = crowdControlEffectsOnCharacter.Contains(CrowdControlEffects.BLIND);
+    }
+}
---
+++ Assets/Scripts/Entities/CrowdControlEffects.cs
@@ -0,0 +1,48 @@
+﻿public enum CrowdControlEffects
+{
+    BLIND,          // Blinded
+    CHARM,          // Charmed + Slowed
+    CRIPPLE,        // -
+    DISARM,         // Disarmed
+    DISRUPT,        // Silenced
+    DROWSY,         // Drowsy
+    ENTANGLE,       // Rooted + Disarmed
+    FEAR,           // Fleeing + Slowed
+    GROUND,         // Grounded
+    KNOCKASIDE,     // Airborne + Stunned
+    KNOCKBACK,      // Airborne + Stunned
+    KNOCKDOWN,      // -
+    KNOCKUP,        // Airborne + Stunned
+    NEARSIGHT,      // Nearsighted
+    PACIFY,         // Silenced + Disarmed
+    POLYMORPH,      // Polymorphed + Pacify
+    PULL,           // Airborne + Stunned
+    ROOT,           // Rooted
+    SLOW,           // Slowed
+    SILENCE,        // Silenced
+    SLEEP,          // Asleep
+    STASIS,         // -
+    STUN,           // Stunned
+    SUPPRESION,     // Suppressed
+    SUSPENSION,     // Stunned
+    TAUNT,          // Taunted
+}
+/*
+    AIRBORNE,       // Target is displaced 
+    BLIND,          // Target's basic attacks do not damage or apply on-hit effects
+    CRIPPLE,        // Target's attack speed is reduced
+    DISARM,         // Target cannot use basic attacks
+    DISRUPT,        // Interrupts target's channeling and charging abilities
+    DROWSY,         // Target will fall asleep after a short delay
+    FORCED,         // Target cannot use movement and is applied a forced move
+    GROUND,         // Target cannot use dashes or blinks
+    KNOCKDOWN,      // Interrupts target's dashes or enemy displacement
+    NEARSIGHT,      // Lowers the vision radius of the target
+    ROOT,           // Target cannot use movement
+    SLOW,           // Target has reduced movement speed
+    SILENCE,        // Target cannot use abilities or items
+    SLEEP,          // Target is stunned until it takes damage
+    STASIS,         // Target is invulnerable and cannot use anything
+    STUN,           // Target cannot use movement, basic attacks, abilities or items
+    SUPPRESSION,    // Target is stunned and cannot use any summoner spells
+*/
