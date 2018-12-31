7b4a3499dc51094251c08e8e3efead17d6fd85a8
MaitreMenard <maxime.menard.1@ulaval.ca>
2018-12-30 23:54:31 -0500

    Unit tests dans Unity wow 10/10

---
+++ Assets/Tests/Units/Stats/ResourceTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class ResourceTest
+    {
+        private static readonly float RESOURCE_INITIAL_BASE_VALUE = 100;
+
+        private Resource resource;
+
+        [SetUp]
+        public void SetUp()
+        {
+            resource = new Resource(RESOURCE_INITIAL_BASE_VALUE);
+        }
+
+        [Test]
+        public void GivenAnAmountGreaterThanCurrentValue_whenReduce_thenCurrentValueBecomesZero()
+        {
+            float amount = 2 * RESOURCE_INITIAL_BASE_VALUE;
+
+            resource.Reduce(amount);
+
+            float currentValue = resource.GetCurrentValue();
+            Assert.AreEqual(0, currentValue);
+        }
+    }
+}
